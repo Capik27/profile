7f39f46d67323b7d19dcc05266f93385deab2922
Konstantin <kaplan.sk8@mail.ru>
2023-10-23 00:10:38 +0300

    hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,25 @@
 import ReactDOM from "react-dom/client";
 import App from "1_app";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import {
+  //BrowserRouter,
+  HashRouter,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "5_shared/store";
 
 const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
-	<>
-		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</Provider>
-	</>
+  <>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </>
 );
 
 reportWebVitals();
