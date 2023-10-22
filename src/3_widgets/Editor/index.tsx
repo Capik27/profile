f010399872e2f77c29cdcddfdacae3e40672d2b7
Konstantin <kaplan.sk8@mail.ru>
2023-10-23 00:44:05 +0300

    autocomplete off

--- src/3_widgets/Editor/index.tsx
+++ src/3_widgets/Editor/index.tsx
@@ -16,165 +16,165 @@ import { useNavigate } from "react-router-dom";
 import { MAIN_ROUTE } from "5_shared/router/paths";
 
 const Editor: React.FC<Props> = ({ data }) => {
-	const [alert, setAlert] = useState<boolean>(false);
-	const [loading, setLoading] = useState<boolean>(false);
-	const [del, setDel] = useState<number>(0);
-	const currentData = useRef<FilesArray | Image[]>(data?.images || []);
-	const [files, setFiles] = useState<FilesArray | Image[]>(data?.images || []);
-	const [title, setTitle] = useState<string>(data?.title || "");
-	const [content, setContent] = useState<string>(data?.description || "");
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
+  const [alert, setAlert] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [del, setDel] = useState<number>(0);
+  const currentData = useRef<FilesArray | Image[]>(data?.images || []);
+  const [files, setFiles] = useState<FilesArray | Image[]>(data?.images || []);
+  const [title, setTitle] = useState<string>(data?.title || "");
+  const [content, setContent] = useState<string>(data?.description || "");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-	const handleTitleChange = (e: SyntheticEvent) => {
-		const value = (e.target as HTMLInputElement).value;
-		setTitle(value);
-	};
-	const handleContentChange = (e: SyntheticEvent) => {
-		const value = (e.target as HTMLInputElement).value;
-		setContent(value);
-	};
-	const handleAlertClose = () => {
-		setAlert(false);
-	};
-	const getAlertMsg = () => {
-		let result;
-		const msg = `Post '${title}' successfully`;
-		if (del) {
-			result = msg + " deleted";
-		} else {
-			result = data ? msg + " modified" : msg + " created";
-		}
-		return result;
-	};
+  const handleTitleChange = (e: SyntheticEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    setTitle(value);
+  };
+  const handleContentChange = (e: SyntheticEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    setContent(value);
+  };
+  const handleAlertClose = () => {
+    setAlert(false);
+  };
+  const getAlertMsg = () => {
+    let result;
+    const msg = `Post '${title}' successfully`;
+    if (del) {
+      result = msg + " deleted";
+    } else {
+      result = data ? msg + " modified" : msg + " created";
+    }
+    return result;
+  };
 
-	const handleDeletePost = () => {
-		if (del) {
-			setLoading(true);
-			deletePost(data?.id, currentData.current);
-			dispatch(setUpdate(true));
-			const timer = setTimeout(() => {
-				clearTimeout(timer);
-				setAlert(true);
-				setLoading(false);
-				setDel(0);
-				navigate(MAIN_ROUTE);
-			}, 1000);
-		} else {
-			setDel(CONFIRM_DELAY);
-		}
-	};
+  const handleDeletePost = () => {
+    if (del) {
+      setLoading(true);
+      deletePost(data?.id, currentData.current);
+      dispatch(setUpdate(true));
+      const timer = setTimeout(() => {
+        clearTimeout(timer);
+        setAlert(true);
+        setLoading(false);
+        setDel(0);
+        navigate(MAIN_ROUTE);
+      }, 1000);
+    } else {
+      setDel(CONFIRM_DELAY);
+    }
+  };
 
-	const handleSubmit = async () => {
-		setLoading(true);
+  const handleSubmit = async () => {
+    setLoading(true);
 
-		if (data) {
-			// change
-			const changedData: changedData = {
-				orderedFiles: files,
-				delete: [],
-			};
+    if (data) {
+      // change
+      const changedData: changedData = {
+        orderedFiles: files,
+        delete: [],
+      };
 
-			const newFileNames: previewNameT[] = files.map(
-				(item: File | Image) => item.name
-			);
+      const newFileNames: previewNameT[] = files.map(
+        (item: File | Image) => item.name
+      );
 
-			currentData.current.forEach((item: Image | File) => {
-				if (!newFileNames.includes(item.name)) {
-					changedData.delete.push(item.name);
-				}
-			});
-			currentData.current = [...files];
+      currentData.current.forEach((item: Image | File) => {
+        if (!newFileNames.includes(item.name)) {
+          changedData.delete.push(item.name);
+        }
+      });
+      currentData.current = [...files];
 
-			await changePost(data, title, content, changedData);
-		} else {
-			//create
-			await uploadPost(title, content, files);
+      await changePost(data, title, content, changedData);
+    } else {
+      //create
+      await uploadPost(title, content, files);
 
-			setTitle("");
-			setContent("");
-			setFiles([]);
-		}
-		setAlert(true);
-		setLoading(false);
-		dispatch(setUpdate(true));
-		navigate(MAIN_ROUTE);
-	};
+      setTitle("");
+      setContent("");
+      setFiles([]);
+    }
+    setAlert(true);
+    setLoading(false);
+    dispatch(setUpdate(true));
+    navigate(MAIN_ROUTE);
+  };
 
-	useEffect(() => {
-		if (del > 0) {
-			const timer = setTimeout(() => {
-				setDel((prev) => prev - 1);
-				clearTimeout(timer);
-			}, 1000);
-		}
-	}, [del]);
+  useEffect(() => {
+    if (del > 0) {
+      const timer = setTimeout(() => {
+        setDel((prev) => prev - 1);
+        clearTimeout(timer);
+      }, 1000);
+    }
+  }, [del]);
 
-	return (
-		<>
-			<Box style={{ display: "grid", justifyItems: "start" }}>
-				<TextInput
-					label="Title"
-					value={title}
-					onChange={handleTitleChange}
-					size="small"
-					margin="dense"
-				/>
-				<TextInput
-					label="Content"
-					value={content}
-					onChange={handleContentChange}
-					multiline
-					minRows={3}
-					fullWidth
-					size="small"
-					margin="dense"
-				/>
-				<ImageForm data={files as Image[]} returnImages={setFiles} />
-				<Box style={{ display: "flex", justifySelf: "end", gap: 8 }}>
-					{data && (
-						<LoadingButton
-							sx={{ width: 150, minWidth: 50 }}
-							variant="contained"
-							color={del ? "warning" : "error"}
-							loading={loading && !!del}
-							disabled={loading}
-							onClick={handleDeletePost}
-						>
-							{del ? `Confirm? ${del}` : "Delete post"}
-						</LoadingButton>
-					)}
-					<LoadingButton
-						variant="contained"
-						loading={loading}
-						disabled={!!del || loading || !title || !content || !files.length}
-						onClick={handleSubmit}
-					>
-						Submit
-					</LoadingButton>
-				</Box>
-			</Box>
-			<Portal>
-				<Snackbar
-					open={alert}
-					autoHideDuration={3000}
-					onClose={handleAlertClose}
-					anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-				>
-					<Alert
-						onClose={handleAlertClose}
-						severity="success"
-						sx={{
-							backgroundColor: "greenligth",
-							border: "1px solid #00000050",
-						}}
-					>
-						{getAlertMsg()}
-					</Alert>
-				</Snackbar>
-			</Portal>
-		</>
-	);
+  return (
+    <>
+      <Box style={{ display: "grid", justifyItems: "start" }}>
+        <TextInput
+          label="Title"
+          value={title}
+          onChange={handleTitleChange}
+          size="small"
+          margin="dense"
+        />
+        <TextInput
+          label="Content"
+          value={content}
+          onChange={handleContentChange}
+          multiline
+          minRows={3}
+          fullWidth
+          size="small"
+          margin="dense"
+        />
+        <ImageForm data={files as Image[]} returnImages={setFiles} />
+        <Box style={{ display: "flex", justifySelf: "end", gap: 8 }}>
+          {data && (
+            <LoadingButton
+              sx={{ width: 150, minWidth: 50 }}
+              variant="contained"
+              color={del ? "warning" : "error"}
+              loading={loading && !!del}
+              disabled={loading}
+              onClick={handleDeletePost}
+            >
+              {del ? `Confirm? ${del}` : "Delete post"}
+            </LoadingButton>
+          )}
+          <LoadingButton
+            variant="contained"
+            loading={loading}
+            disabled={!!del || loading || !title || !content || !files.length}
+            onClick={handleSubmit}
+          >
+            Submit
+          </LoadingButton>
+        </Box>
+      </Box>
+      <Portal>
+        <Snackbar
+          open={alert}
+          autoHideDuration={3000}
+          onClose={handleAlertClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        >
+          <Alert
+            onClose={handleAlertClose}
+            severity="success"
+            sx={{
+              backgroundColor: "greenligth",
+              border: "1px solid #00000050",
+            }}
+          >
+            {getAlertMsg()}
+          </Alert>
+        </Snackbar>
+      </Portal>
+    </>
+  );
 };
 
 export default Editor;
--- src/4_features/TextInput/index.tsx
+++ src/4_features/TextInput/index.tsx
@@ -3,20 +3,27 @@ import { TextField } from "@mui/material";
 import { SyntheticEvent, useState, useEffect } from "react";
 
 const TextInput: React.FC<ControlledTextInputProps> = (props) => {
-	const [value, setValue] = useState<string>("");
+  const [value, setValue] = useState<string>("");
 
-	const handleChange = (e: SyntheticEvent) => {
-		const value = (e.target as HTMLInputElement).value;
-		setValue(value);
-		!!props.onChange && props.onChange(e);
-	};
+  const handleChange = (e: SyntheticEvent) => {
+    const value = (e.target as HTMLInputElement).value;
+    setValue(value);
+    !!props.onChange && props.onChange(e);
+  };
 
-	useEffect(() => {
-		setValue(props.value as string);
-		// console.log(props.label, " // value - ", value);
-	}, [props.value]);
+  useEffect(() => {
+    setValue(props.value as string);
+    // console.log(props.label, " // value - ", value);
+  }, [props.value]);
 
-	return <TextField {...props} value={value} onChange={handleChange} />;
+  return (
+    <TextField
+      {...props}
+      value={value}
+      onChange={handleChange}
+      autoComplete="off"
+    />
+  );
 };
 
 export default TextInput;
