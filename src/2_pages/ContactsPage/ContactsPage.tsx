05bdfb3d90c729e7f08409a7668ddcfc1ac6888f
Konstantin <kaplan.sk8@mail.ru>
2023-12-17 13:05:37 +0300

    upd about/contacts

--- src/2_pages/ContactsPage/ContactsPage.tsx
+++ src/2_pages/ContactsPage/ContactsPage.tsx
@@ -4,11 +4,25 @@ import useFindPost from "5_shared/hooks/useFindPost";
 import { ERROR_ROUTE } from "5_shared/router/paths";
 import { Navigate, useParams } from "react-router-dom";
 import style from "./ContactsPage.module.scss";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import EmailIcon from "@mui/icons-material/Email";
 
 export const ContactsPage: React.FC = () => {
-	return (
-		<>
-			<div>ContactsPage</div>
-		</>
-	);
+    return (
+        <>
+            <h1 className={style.title}>Contacts</h1>
+            <div className={style.links_container}>
+                <a href="mailto:ShikhovKS@gmail.com" className={style.link}>
+                    <EmailIcon className={style.icon} />
+                </a>
+                <a
+                    href="https://telegram.me/Kaplan31"
+                    target="_black"
+                    className={style.link}
+                >
+                    <TelegramIcon className={style.icon} />
+                </a>
+            </div>
+        </>
+    );
 };
